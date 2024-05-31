4db126b234de349e9c30871c3f29d9cf1702e28f
Alexei Gopkalo <vn.gopkalo1978@gmail.com>
2024-05-31 16:58:37 +0300

    change imports in settings

--- project_recipe/settings.py
+++ project_recipe/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,9 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
