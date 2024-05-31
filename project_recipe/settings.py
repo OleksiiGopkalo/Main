b247af158e14629f0d047cd1613acb8ed7ab1c61
Alexei Gopkalo <vn.gopkalo1978@gmail.com>
2024-05-31 16:56:19 +0300

    sercet key add

--- project_recipe/settings.py
+++ project_recipe/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9jo7t)*7&hff$4x9j%37qy(o+&ywxxm@7*@b##!4#f%kxg(_-l'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
