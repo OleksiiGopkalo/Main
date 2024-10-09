75332d3a84d58a2441177841ee8bafda47c40e9d
Alexei Gopkalo <oleksi_gopkalo@knu.ua>
2024-10-09 22:11:49 +0300

    1

--- Lab1/Lab1Tests/UnitTest1.cs
+++ Lab1/Lab1Tests/UnitTest1.cs
@@ -1,10 +1,10 @@
 using System.IO;
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-[TestFixture]
+[TestClass]
 public class DivisorTests
 {
-    [SetUp]
+    [TestInitialize]
     public void SetUp()
     {
         if (File.Exists("OUTPUT.TXT"))
@@ -13,7 +13,7 @@ public class DivisorTests
         }
     }
 
-    [Test]
+    [TestMethod]
     public void TestSmallNumber()
     {
         File.WriteAllText("INPUT.TXT", "12"); // Прості дільники: 2, 3
@@ -22,7 +22,7 @@ public class DivisorTests
         Assert.AreEqual("2", result); // Очікуємо 2 дільники (6 та 12)
     }
 
-    [Test]
+    [TestMethod]
     public void TestPrimeNumber()
     {
         File.WriteAllText("INPUT.TXT", "13"); // Прості дільники: 13
@@ -31,7 +31,7 @@ public class DivisorTests
         Assert.AreEqual("1", result); // Очікуємо 1 дільник (13)
     }
 
-    [Test]
+    [TestMethod]
     public void TestCompositeNumberWithMultiplePrimeFactors()
     {
         File.WriteAllText("INPUT.TXT", "30"); // Прості дільники: 2, 3, 5
@@ -40,16 +40,15 @@ public class DivisorTests
         Assert.AreEqual("1", result); // Очікуємо 1 дільник (30)
     }
 
-    [Test]
-    public void TestLargeCompositeNumber()
-    {
-        File.WriteAllText("INPUT.TXT", "100"); // Прості дільники: 2, 5
-        Program.Main();
-        string result = File.ReadAllText("OUTPUT.TXT");
-        Assert.AreEqual("3", result); // Очікуємо 3 дільники (20, 50, 100)
-    }
-
-    [Test]
+ [TestMethod]
+public void TestLargeCompositeNumber()
+{
+    File.WriteAllText("INPUT.TXT", "100"); // Прості дільники: 2, 5
+    Program.Main();
+    string result = File.ReadAllText("OUTPUT.TXT");
+    Assert.AreEqual("3", result); // Очікуємо 3 дільники (20, 50, 100)
+}
+    [TestMethod]
     public void TestSingleDigitNumber()
     {
         File.WriteAllText("INPUT.TXT", "7"); // Прості дільники: 7
