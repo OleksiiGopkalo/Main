0eb6259d2d3435d1edb0a08d239f2fa431a4a0be
Alexei Gopkalo <oleksi_gopkalo@knu.ua>
2024-10-09 22:15:38 +0300

    1

--- Lab1/Lab1Tests/obj/Debug/net8.0/Lab1Tests.AssemblyInfo.cs
+++ Lab1/Lab1Tests/obj/Debug/net8.0/Lab1Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75332d3a84d58a2441177841ee8bafda47c40e9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+914bab56c9a10d205135d1074fa3a5de16755bc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
