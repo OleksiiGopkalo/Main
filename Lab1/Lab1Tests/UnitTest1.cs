1c0d2d0489adaa7bbf682bf28b8e71951adb1ec4
Alexei Gopkalo <oleksi_gopkalo@knu.ua>
2024-10-09 22:06:25 +0300

    1

--- Lab1/Lab1Tests/UnitTest1.cs
+++ Lab1/Lab1Tests/UnitTest1.cs
@@ -1,10 +1,60 @@
-namespace Lab1Tests;
+using System.IO;
+using NUnit.Framework;
 
-public class UnitTest1
+[TestFixture]
+public class DivisorTests
 {
-    [Fact]
-    public void Test1()
+    [SetUp]
+    public void SetUp()
     {
+        if (File.Exists("OUTPUT.TXT"))
+        {
+            File.Delete("OUTPUT.TXT");
+        }
+    }
 
+    [Test]
+    public void TestSmallNumber()
+    {
+        File.WriteAllText("INPUT.TXT", "12"); // Прості дільники: 2, 3
+        Program.Main();
+        string result = File.ReadAllText("OUTPUT.TXT");
+        Assert.AreEqual("2", result); // Очікуємо 2 дільники (6 та 12)
+    }
+
+    [Test]
+    public void TestPrimeNumber()
+    {
+        File.WriteAllText("INPUT.TXT", "13"); // Прості дільники: 13
+        Program.Main();
+        string result = File.ReadAllText("OUTPUT.TXT");
+        Assert.AreEqual("1", result); // Очікуємо 1 дільник (13)
+    }
+
+    [Test]
+    public void TestCompositeNumberWithMultiplePrimeFactors()
+    {
+        File.WriteAllText("INPUT.TXT", "30"); // Прості дільники: 2, 3, 5
+        Program.Main();
+        string result = File.ReadAllText("OUTPUT.TXT");
+        Assert.AreEqual("1", result); // Очікуємо 1 дільник (30)
+    }
+
+    [Test]
+    public void TestLargeCompositeNumber()
+    {
+        File.WriteAllText("INPUT.TXT", "100"); // Прості дільники: 2, 5
+        Program.Main();
+        string result = File.ReadAllText("OUTPUT.TXT");
+        Assert.AreEqual("3", result); // Очікуємо 3 дільники (20, 50, 100)
+    }
+
+    [Test]
+    public void TestSingleDigitNumber()
+    {
+        File.WriteAllText("INPUT.TXT", "7"); // Прості дільники: 7
+        Program.Main();
+        string result = File.ReadAllText("OUTPUT.TXT");
+        Assert.AreEqual("1", result); // Очікуємо 1 дільник (7)
     }
 }
