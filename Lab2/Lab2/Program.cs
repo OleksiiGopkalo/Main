021c1d87ae408c2abbfc00754e98d8c33b19a1b9
Alexei Gopkalo <oleksi_gopkalo@knu.ua>
2024-10-09 22:51:24 +0300

    1

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] input = File.ReadAllLines("INPUT.TXT");
+        string[] parameters = input[0].Split();
+        
+        int n = int.Parse(parameters[0]);
+        int k = int.Parse(parameters[1]);
+        string s = input[1];
+
+        int result = CountAlmostPalindromicSubstrings(s, n, k);
+        File.WriteAllText("OUTPUT.TXT", result.ToString());
+    }
+
+    public static int CountAlmostPalindromicSubstrings(string s, int n, int k)
+    {
+        int count = 0;
+
+        // Генеруємо всі можливі підслів'я
+        for (int start = 0; start < n; start++)
+        {
+            for (int end = start; end < n; end++)
+            {
+                string substring = s.Substring(start, end - start + 1);
+                int changesRequired = CountChangesToMakePalindrome(substring);
+                
+                if (changesRequired <= k)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static int CountChangesToMakePalindrome(string str)
+    {
+        int changes = 0;
+        int len = str.Length;
+        
+        for (int i = 0; i < len / 2; i++)
+        {
+            if (str[i] != str[len - i - 1])
+            {
+                changes++;
+            }
+        }
+
+        return changes;
+    }
+}
--- Lab2/Lab2Tests/UnitTest1.cs
+++ Lab2/Lab2Tests/UnitTest1.cs
@@ -1,10 +1,50 @@
-namespace Lab2Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-public class UnitTest1
+[TestClass]
+public class ProgramTests
 {
-    [Fact]
-    public void Test1()
+    [TestMethod]
+    public void TestExample1()
     {
+        string s = "abcde";
+        int n = 5, k = 1;
+        int result = Program.CountAlmostPalindromicSubstrings(s, n, k);
+        Assert.AreEqual(12, result);
+    }
 
+    [TestMethod]
+    public void TestExample2()
+    {
+        string s = "aaa";
+        int n = 3, k = 3;
+        int result = Program.CountAlmostPalindromicSubstrings(s, n, k);
+        Assert.AreEqual(6, result);
+    }
+
+    [TestMethod]
+    public void TestSingleCharacter()
+    {
+        string s = "a";
+        int n = 1, k = 0;
+        int result = Program.CountAlmostPalindromicSubstrings(s, n, k);
+        Assert.AreEqual(1, result); // Лише одна підстрока "a" і вона вже паліндром
+    }
+
+    [TestMethod]
+    public void TestAllSameCharacters()
+    {
+        string s = "aaaa";
+        int n = 4, k = 0;
+        int result = Program.CountAlmostPalindromicSubstrings(s, n, k);
+        Assert.AreEqual(10, result); // Усі підстроки вже є паліндромами
+    }
+
+    [TestMethod]
+    public void TestMaximumK()
+    {
+        string s = "abcd";
+        int n = 4, k = 2;
+        int result = Program.CountAlmostPalindromicSubstrings(s, n, k);
+        Assert.AreEqual(10, result); // Усі підстроки можна перетворити на паліндроми з K = 2
     }
 }
