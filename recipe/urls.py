6483b8f4b2b62046e19312709ef38ac4ef52ce32
Alexei Gopkalo <vn.gopkalo1978@gmail.com>
2024-10-09 20:33:41 +0300

    delete

--- recipe/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from recipe import views
-
-urlpatterns = [
-  path('', views.main, name='main'),
-  path('category_detail/', views.category_detail, name='category_detail'),
-]
